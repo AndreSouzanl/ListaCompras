40e2fe54dcd12bf97016c3eee0dcbe0f9e4979bb
Andresouzanl <devals2022@gmail.com>
2023-09-30 18:32:53 -0300

    Atualizando

--- main/main.js
+++ main/main.js
@@ -1,68 +1,108 @@
-const itemProduto = [];
-const numeroCelular = "+5531984055730";
+    const itemProduto = [];
+    const numeroCelular = "+5531984055730";
+    let modoEdicao = false;
+    let itemEditandoIndex = null;
 
-document
-  .getElementById("adicionarProduto")
-  .addEventListener("click", function () {
-    const nomeProduto = document.getElementById("nomeProduto").value;
-    const quantidadeProduto =
-      document.getElementById("quantidadeProduto").value;
+    document
+      .getElementById("adicionarProduto")
+      .addEventListener("click", function () {
+        const nomeProduto = document.getElementById("nomeProduto").value;
+        const quantidadeProduto =
+          document.getElementById("quantidadeProduto").value;
 
-    if (nomeProduto === "" || quantidadeProduto === "") {
-      alert("Por Favor ! Preencha todos os campos");
-      return;
+        if (modoEdicao) {
+          // Editar o item existente
+          itemProduto[itemEditandoIndex].nomeProduto = nomeProduto;
+          itemProduto[itemEditandoIndex].quantidadeProduto = quantidadeProduto;
+          modoEdicao = false;
+          itemEditandoIndex = null;
+          atualizarTabela();
+        } else {
+          // Adicionar um novo item
+          if (nomeProduto.trim() === "" || quantidadeProduto.trim() === "") {
+            alert("Por favor, preencha todos os campos.");
+            return;
+          }
+          const itensProduto = {
+            nomeProduto,
+            quantidadeProduto,
+          };
+          itemProduto.push(itensProduto);
+          atualizarTabela();
+        }
+
+        document.getElementById("nomeProduto").value = "";
+        document.getElementById("quantidadeProduto").value = "";
+      });
+
+    function EnviarWhattapp() {
+      if (itemProduto.length === 0) {
+        alert("Nenhum produto cadastrado para enviar via WhatsApp.");
+        return;
+      }
+
+      let mensagem = "Lista de Produtos:\n";
+
+      for (let i = 0; i < itemProduto.length; i++) {
+        mensagem += `Produto: ${itemProduto[i].nomeProduto},\nQuantidade: ${itemProduto[i].quantidadeProduto}\n`;
+      }
+
+      const numeroWhatsApp = numeroCelular;
+
+      // Encode a mensagem para que seja uma URL válida
+      mensagem = encodeURIComponent(mensagem);
+
+      // Crie o link WhatsApp com o número de telefone e a mensagem
+      const linkWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${mensagem}`;
+
+      window.open(linkWhatsApp, "_blank");
     }
 
-    const itensProduto = {
-      nomeProduto: nomeProduto,
-      quantidadeProduto: quantidadeProduto,
-    };
+    function editarItem(index) {
+      const item = itemProduto[index];
+      document.getElementById("nomeProduto").value = item.nomeProduto;
+      document.getElementById("quantidadeProduto").value = item.quantidadeProduto;
+      modoEdicao = true;
+      itemEditandoIndex = index;
+    }
 
-    itemProduto.push(itensProduto);
+    function excluirItem(index) {
+      itemProduto.splice(index, 1);
+      atualizarTabela();
+    }
 
-   const tabela = document
-      .getElementById("ProdutosCadastrados")
-      .getElementsByTagName("tbody")[0];
+    function atualizarTabela() {
+      const tabela = document.getElementById("ProdutosCadastrados");
+      const tbody = tabela.getElementsByTagName("tbody")[0];
+      tbody.innerHTML = "";
 
-    // Cria uma nova linha (tr)
-    const novaLinha = tabela.insertRow();
+      for (let i = 0; i < itemProduto.length; i++) {
+        const item = itemProduto[i];
+        const novaLinha = tbody.insertRow();
+        const celularProduto = novaLinha.insertCell(0);
+        const celulaQuantidade = novaLinha.insertCell(1);
+        const acoes = novaLinha.insertCell(2);
+        celularProduto.textContent = item.nomeProduto;
+        celulaQuantidade.textContent = item.quantidadeProduto;
 
-    // Cria células (td) para a linha
-    const celularProduto = novaLinha.insertCell(0);
-    const celulaQuantidade = novaLinha.insertCell(1);
+        const botaoEditar = document.createElement("button");
+        botaoEditar.textContent = "Editar";
+        botaoEditar.classList.add("botao", "editar-botao");
+        botaoEditar.addEventListener("click", () => editarItem(i));
 
-    // Define o conteúdo das células com os valores inseridos
-    celularProduto.textContent = nomeProduto;
-    celulaQuantidade.textContent = quantidadeProduto;
+        const botaoExcluir = document.createElement("button");
+        botaoExcluir.textContent = "Excluir";
+        botaoExcluir.classList.add("botao-red", "excluir-botao");
+        botaoExcluir.addEventListener("click", () => excluirItem(i));
 
-    document.getElementById("nomeProduto").value = " ";
-    document.getElementById("quantidadeProduto").value = " ";
-  });
+        const divAcoes = document.createElement("div");
+        divAcoes.classList.add("acao-botao");
+        divAcoes.appendChild(botaoEditar);
+        divAcoes.appendChild(botaoExcluir);
 
-function EnviarWhattapp() {
-  
-  if (itemProduto.length === 0) {
-    alert("Nenhum produto cadastrado para enviar no WhatsApp.");
-    return;
-  }
+        acoes.appendChild(divAcoes);
+      }
+    }
 
-  let mensagem = "Lista de Produtos:\n";
-
-  for (let i = 0; i < itemProduto.length; i++) {
-    mensagem += ` Produto: ${itemProduto[i].nomeProduto},
-    Quantidade: ${itemProduto[i].quantidadeProduto}\n`;
-  }
-
-  const numeroWhatsApp = numeroCelular;
-
-  // Encode a mensagem para que seja uma URL válida
-  mensagem = encodeURIComponent(mensagem);
-
-  // Crie o link WhatsApp com o número de telefone e a mensagem
-  const linkWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${mensagem}`;
-
-  window.open(linkWhatsApp, "_blank");
-}
-
-const botaoEnviar = document.getElementById("botaoEnviar");
-botaoEnviar.addEventListener("click", EnviarWhattapp);
+    const botaoEnviar = document.getElementById("botaoEnviar");
+    botaoEnviar.addEventListener("click", EnviarWhattapp);
