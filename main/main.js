21d6ca39c87c9887f0aca24ab12d39f7c995b9d8
Bowie <devals2022@gmail.com>
2023-11-24 22:02:28 -0300

    Inserindonovafuncionalidade

--- main/main.js
+++ main/main.js
@@ -1,109 +1,126 @@
-    const itemProduto = [];
-    const numeroCelular = "+5531984055730";
-    let modoEdicao = false;
-    let itemEditandoIndex = null;
+let itemProduto = [];
+const numeroCelular = "+5531984055730";
+let modoEdicao = false;
+let itemEditandoIndex = null;
 
-      document.getElementById("adicionarProduto").addEventListener("click", function () {
-        const nomeProduto = document.getElementById("nomeProduto").value;
-        const quantidadeProduto =
-          document.getElementById("quantidadeProduto").value;
+// Carregar dados do localStorage no início
+const dadosArmazenados = localStorage.getItem('produtos');
 
-        if (modoEdicao) {
-          // Editar o item existente
-          itemProduto[itemEditandoIndex].nomeProduto = nomeProduto;
-          itemProduto[itemEditandoIndex].quantidadeProduto = quantidadeProduto;
-          modoEdicao = false;
-          itemEditandoIndex = null;
-          atualizarTabela();
-        } else {
-          // Adicionar um novo item
-          if (nomeProduto.trim() === "" || quantidadeProduto.trim() === "") {
-            alert("Por favor, preencha todos os campos.");
-            return;
-          }
-          const itensProduto = {
-            nomeProduto,
-            quantidadeProduto,
-          };
-          itemProduto.push(itensProduto);
-          atualizarTabela();
-        }
+if (dadosArmazenados) {
+  // Se houver dados armazenados, converte de JSON para o formato de objeto JavaScript
+  itemProduto = JSON.parse(dadosArmazenados);
+  atualizarTabela();
+}
 
-        document.getElementById("nomeProduto").value = "";
-        document.getElementById("quantidadeProduto").value = "";
-      });
+document
+  .getElementById("adicionarProduto")
+  .addEventListener("click", function () {
+    const nomeProduto = document.getElementById("nomeProduto").value;
+    const quantidadeProduto =
+      document.getElementById("quantidadeProduto").value;
 
-
-      function EnviarWhattapp() {
-        if (itemProduto.length === 0) {
-          alert("Nenhum produto cadastrado para enviar via WhatsApp.");
-          return;
-        }
-  
-        let mensagem = "Lista de Produtos:\n";
-  
-        for (let i = 0; i < itemProduto.length; i++) {
-          mensagem += `Produto: ${itemProduto[i].nomeProduto},\nQuantidade: ${itemProduto[i].quantidadeProduto}\n`;
-        }
-  
-        const numeroWhatsApp = numeroCelular;
-  
-        // Encode a mensagem para que seja uma URL válida
-        mensagem = encodeURIComponent(mensagem);
-  
-        // Crie o link WhatsApp com o número de telefone e a mensagem
-        const linkWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${mensagem}`;
-  
-        window.open(linkWhatsApp, "_blank");
+    if (modoEdicao) {
+      // Editar o item existente
+      itemProduto[itemEditandoIndex].nomeProduto = nomeProduto;
+      itemProduto[itemEditandoIndex].quantidadeProduto = quantidadeProduto;
+      modoEdicao = false;
+      itemEditandoIndex = null;
+      atualizarTabela();
+    } else {
+      // Adicionar um novo item
+      if (nomeProduto.trim() === "" || quantidadeProduto.trim() === "") {
+        alert("Por favor, preencha todos os campos.");
+        return;
       }
-
-    
-
-    function editarItem(index) {
-      const item = itemProduto[index];
-      document.getElementById("nomeProduto").value = item.nomeProduto;
-      document.getElementById("quantidadeProduto").value = item.quantidadeProduto;
-      modoEdicao = true;
-      itemEditandoIndex = index;
-    }
-
-    function excluirItem(index) {
-      itemProduto.splice(index, 1);
+      const itensProduto = {
+        nomeProduto,
+        quantidadeProduto,
+      };
+      itemProduto.push(itensProduto);
       atualizarTabela();
     }
 
-    function atualizarTabela() {
-      const tabela = document.getElementById("ProdutosCadastrados");
-      const tbody = tabela.getElementsByTagName("tbody")[0];
-      tbody.innerHTML = "";
+    document.getElementById("nomeProduto").value = "";
+    document.getElementById("quantidadeProduto").value = "";
+  });
 
-      for (let i = 0; i < itemProduto.length; i++) {
-        const item = itemProduto[i];
-        const novaLinha = tbody.insertRow();
-        const celularProduto = novaLinha.insertCell(0);
-        const celulaQuantidade = novaLinha.insertCell(1);
-        const acoes = novaLinha.insertCell(2);
-        celularProduto.textContent = item.nomeProduto;
-        celulaQuantidade.textContent = item.quantidadeProduto;
+// function EnviarWhattapp() {
+//   if (itemProduto.length === 0) {
+//     alert("Nenhum produto cadastrado para enviar via WhatsApp.");
+//     return;
+//   }
 
-        const botaoEditar = document.createElement("button");
-        botaoEditar.textContent = "Editar";
-        botaoEditar.classList.add("botao", "editar-botao");
-        botaoEditar.addEventListener("click", () => editarItem(i));
+//   let mensagem = "Lista de Produtos:\n";
 
-        const botaoExcluir = document.createElement("button");
-        botaoExcluir.textContent = "Excluir";
-        botaoExcluir.classList.add("botao-red", "excluir-botao");
-        botaoExcluir.addEventListener("click", () => excluirItem(i));
+//   for (let i = 0; i < itemProduto.length; i++) {
+//     mensagem += `Produto: ${itemProduto[i].nomeProduto},\nQuantidade: ${itemProduto[i].quantidadeProduto}\n`;
+//   }
 
-        const divAcoes = document.createElement("div");
-        divAcoes.classList.add("acao-botao");
-        divAcoes.appendChild(botaoEditar);
-        divAcoes.appendChild(botaoExcluir);
+//   const numeroWhatsApp = numeroCelular;
 
-        acoes.appendChild(divAcoes);
-      }
-    }
+//   // Encode a mensagem para que seja uma URL válida
+//   mensagem = encodeURIComponent(mensagem);
 
-    const botaoEnviar = document.getElementById("botaoEnviar");
-    botaoEnviar.addEventListener("click", EnviarWhattapp);
+//   // Crie o link WhatsApp com o número de telefone e a mensagem
+//   const linkWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${mensagem}`;
+
+//   window.open(linkWhatsApp, "_blank");
+// }
+
+function editarItem(index) {
+  const item = itemProduto[index];
+  document.getElementById("nomeProduto").value = item.nomeProduto;
+  document.getElementById("quantidadeProduto").value = item.quantidadeProduto;
+  modoEdicao = true;
+  itemEditandoIndex = index;
+}
+
+function excluirItem(index) {
+  itemProduto.splice(index, 1);
+  atualizarTabela();
+}
+
+function atualizarTabela() {
+  const tabela = document.getElementById("ProdutosCadastrados");
+  const tbody = tabela.getElementsByTagName("tbody")[0];
+  tbody.innerHTML = "";
+
+  for (let i = 0; i < itemProduto.length; i++) {
+    const item = itemProduto[i];
+    const novaLinha = tbody.insertRow();
+    const celularProduto = novaLinha.insertCell(0);
+    const celulaQuantidade = novaLinha.insertCell(1);
+    const acoes = novaLinha.insertCell(2);
+    celularProduto.textContent = item.nomeProduto;
+    celulaQuantidade.textContent = item.quantidadeProduto;
+
+    const botaoEditar = document.createElement("button");
+    botaoEditar.textContent = "Editar";
+    botaoEditar.classList.add("botao", "editar-botao");
+    botaoEditar.addEventListener("click", () => editarItem(i));
+
+    const botaoExcluir = document.createElement("button");
+    botaoExcluir.textContent = "Excluir";
+    botaoExcluir.classList.add("botao-red", "excluir-botao");
+    botaoExcluir.addEventListener("click", () => excluirItem(i));
+
+    const divAcoes = document.createElement("div");
+    divAcoes.classList.add("acao-botao");
+    divAcoes.appendChild(botaoEditar);
+    divAcoes.appendChild(botaoExcluir);
+
+    acoes.appendChild(divAcoes);
+  }
+
+  // Adicione a chamada para atualizarLocalStorage() ao final da função
+  atualizarLocalStorage();
+}
+
+const botaoEnviar = document.getElementById("botaoEnviar");
+botaoEnviar.addEventListener("click", EnviarWhattapp);
+
+// Nova função para atualizar o localStorage
+function atualizarLocalStorage() {
+  // Converte o array de itens para JSON e armazena no localStorage
+  localStorage.setItem('produtos', JSON.stringify(itemProduto));
+}
