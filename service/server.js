a1a816e2371b96ae328cb7fbe552d6b68e1bdfa3
Andresouzanl <devals2022@gmail.com>
2024-08-30 17:52:54 -0300

    atualizado!

--- service/server.js
+++ service/server.js
@@ -4,11 +4,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: "https://lista-compras-eight.vercel.app/",
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
